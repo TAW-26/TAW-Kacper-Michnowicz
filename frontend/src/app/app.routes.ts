ada6f8eafa116c675ba99ca68fc3a4769bf91fbb
KacperM122 <kmichnowicz122@gmail.com>
2026-04-30 20:20:25 +0200

    Dodanie widoku szczegółów

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 import { LoginComponent } from './login/login';
 import { HomeComponent } from './home/home';
+import { CourtDetailsComponent } from './court-details/court-details';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'court/:id', component: CourtDetailsComponent },
 ];
