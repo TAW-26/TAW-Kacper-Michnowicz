068c139beb6c311bb925e29a09ead8ad60ff9606
KacperM122 <kmichnowicz122@gmail.com>
2026-06-11 18:29:14 +0200

    poprawki

--- frontend/src/app/court-details/court-details.ts
+++ frontend/src/app/court-details/court-details.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CourtService } from '../court'; // Upewnij się, że ścieżka do serwisu jest poprawna
 
 @Component({
   selector: 'app-court-details',
@@ -11,36 +12,43 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
   styleUrl: './court-details.css'
 })
 export class CourtDetailsComponent implements OnInit {
-  courtId: string | null = null;
+  court: any = null; // Początkowo null, zmieni się po pobraniu danych
+  courtId: string = '';
 
-  court = {
-    name: 'Orlik przy Szkole nr 5',
-    type: 'Piłka nożna',
-    description: 'Nowoczesne boisko ze sztuczną nawierzchnią i oświetleniem LED.',
-    price: 50,
-    address: 'ul. Sportowa 12, Warszawa'
-  };
-
+  // Obiekt rezerwacji powiązany z formularzem HTML
   reservation = {
     date: '',
     startTime: '',
     endTime: ''
   };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private courtService: CourtService
+  ) {}
 
   ngOnInit() {
-    this.courtId = this.route.snapshot.paramMap.get('id');
+    // 1. Wyciągamy dynamiczne ID z paska adresu URL (np. /court/123)
+    this.courtId = this.route.snapshot.paramMap.get('id') || '';
+    console.log('Odczytane ID obiektu z URL:', this.courtId);
+
+    if (this.courtId) {
+      // 2. Strzelamy do backendu po konkretny obiekt z bazy danych
+      this.courtService.getCourtById(this.courtId).subscribe({
+        next: (data) => {
+          console.log('Dane obiektu pobrane pomyślnie:', data);
+          this.court = data; // Ustawienie tej zmiennej "ożywi" plik HTML
+        },
+        error: (err) => {
+          console.error('Błąd podczas pobierania szczegółów obiektu:', err);
+        }
+      });
+    }
   }
 
+  // Funkcja uruchamiana po wysłaniu formularza
   onSubmitReservation() {
-    const payload = {
-      courtId: this.courtId,
-      start: `${this.reservation.date}T${this.reservation.startTime}:00.000Z`,
-      end: `${this.reservation.date}T${this.reservation.endTime}:00.000Z`
-    };
-
-    console.log('Wysyłanie rezerwacji:', payload);
-    alert('Rezerwacja została wysłana! Sprawdź panel użytkownika.');
+    console.log('Wysyłanie rezerwacji:', this.reservation);
+    // Tutaj możesz dopisać logikę wysyłania rezerwacji do serwera (np. this.courtService.createReservation)
   }
 }
